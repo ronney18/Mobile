db13cb2faa4e336ef24c0d9ea169c622d800cc2e
ronney18 <Ronney_Sanchez@student.uml.edu>
2019-11-14 18:11:58 -0500

    Updated Files

--- app/src/main/java/com/example/umlmentor/MainActivity.java
+++ app/src/main/java/com/example/umlmentor/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.umlmentor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+    Button btnLogin;
+    EditText etUserName, etPassword;
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUserName = (EditText) findViewById(R.id.etUserName);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnSignUp = (Button) findViewById(R.id.btn_sign_up);
+
+        btnLogin.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnLogin:
+                break;
+
+            case R.id.btn_sign_up:
+                Intent gotoSecond = new Intent();
+                gotoSecond.setClass(this, Register.class);
+                startActivity(gotoSecond);
+                break;
+
+        }
+
     }
 }
