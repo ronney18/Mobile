fae69c43d1e65f3930afa1622f131575d4c0b3f6
ronney18 <Ronney_Sanchez@student.uml.edu>
2019-11-24 20:43:26 -0500

    Updates

--- app/src/main/java/com/example/umlmentor/MainActivity.java
+++ app/src/main/java/com/example/umlmentor/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.umlmentor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,34 +8,74 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnLogin;
-    EditText etUserName, etPassword;
+    private Button login;
+    private EditText userName, password;
     private Button signUp;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etUserName = (EditText) findViewById(R.id.etUserName);
-        etPassword = (EditText) findViewById(R.id.etPassword);
-        btnLogin = (Button) findViewById(R.id.btnLogin);
+        userName = (EditText) findViewById(R.id.etUserName);
+        password = (EditText) findViewById(R.id.etPassword);
+        login = (Button) findViewById(R.id.btnLogin);
         signUp = (Button) findViewById(R.id.btnSignUp);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openRegister();
+                finish();
+                startActivity(new Intent(MainActivity.this, Register.class));
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(userName.getText().toString().isEmpty() || password.getText().toString().isEmpty())
+                {
+                    Toast.makeText(MainActivity.this,"No Username and/or password!",
+                            Toast.LENGTH_LONG).show();
+                }
+                else {
+                    validate(userName.getText().toString(), password.getText().toString().trim());
+                }
             }
         });
 
     }
 
-    private void openRegister() {
-        Intent intent = new Intent();
-        intent.setClass(MainActivity.this, Register.class);
-        startActivity(intent);
+    private void validate(String userName, String password) {
+        firebaseAuth.signInWithEmailAndPassword(userName, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(MainActivity.this,"Login Successful!",
+                                    Toast.LENGTH_LONG).show();
+                            finish();
+                            startActivity(new Intent(MainActivity.this, Homepage.class));
+                        }
+
+                        else
+                        {
+                            Toast.makeText(MainActivity.this,
+                                    "Invalid Username and/or Password!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
